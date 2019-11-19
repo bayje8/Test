13856285491a794e84aefcb39e8b1ff2386081d8
bayje8 <k.j.balachandar@gmail.com>
2019-11-19 15:42:08 -0500

    intial commit

---
+++ src/com/first/First.java
@@ -0,0 +1,17 @@
+package com.first;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my PC...:)");
+
+		First first = new First();
+		first.collectionTutorial();
+	}
+
+	private void collectionTutorial() {
+
+	}
+
+}
